fb260f1b1621b56d7b96ee58a0359af38f798e97
Marcus Asplund <rootfood@gmail.com>
2018-10-11 23:52:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js')
     .then(function (registration) {
       console.log('Service Worker registration successful with scope: ',
-      registration.scope)
+        registration.scope)
     })
     .catch(function (err) {
       console.log('Service Worker registration failed: ', err)
